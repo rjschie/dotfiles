bc3644d0f653af00324a2dfbfaad3dc99030491a
Ryan Schie <rjschie@gmail.com>
2026-04-08 10:36:49 +0100

    fix(fish): update psef function for better output

--- files/fish/functions/psef.fish
+++ files/fish/functions/psef.fish
@@ -1,3 +1,29 @@
 function psef
-  ps -ef | grep -v -e "/System" -e "/Applications" -e "/usr/libexec" -e "/usr/sbin" -e "/usr/bin/login" -e "/bin/fish"
+  set -l exclude \
+    "/System" \
+    "/Applications" \
+    "/usr/libexec" \
+    "/usr/sbin" \
+    "/sbin" \
+    "automountd" \
+    "endpointsecurityd" \
+    "autofsd" \
+    "aslmanager" \
+    "cloudphotod" \
+    "trustevaluationagent" \
+    "/usr/bin/login" \
+    "/bin/fish" \
+    "fish" \
+    "ps -ef" \
+    "Core Audio Driver" \
+    "/CleverFiles" \
+    "/com.tunabelly" \
+    "com.1password" \
+    "cc-telemetry" \
+    "grafana" \
+    "/nvim"
+  ps -ef | grep -Ev (string join '|' $exclude) | awk '
+    { if (NR == 1) print "\033[1m" $0 "\033[0m"
+      else if (NR % 2 == 0) print "\033[36m" $0 "\033[0m"
+      else print "\033[34m" $0 "\033[0m" }'
 end
