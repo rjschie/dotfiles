4c1066d48c289dbe67a727506c3299c7072df811
Ryan Schie <rjschie@gmail.com>
2026-04-26 12:51:12 +0100

    fix(fish): update XDG variables

--- files/fish/conf.d/1-shell-setup.fish
+++ files/fish/conf.d/1-shell-setup.fish
@@ -3,6 +3,8 @@ if status is-interactive
   set -gx CODE $HOME/code
   set -gx CONFIG $HOME/.config
   set -gx XDG_CONFIG_HOME $HOME/.config
+  set -gx XDG_DATA_HOME $HOME/.local/share
+  set -gx XDG_STATE_HOME $HOME/.local/state
   set -gx DOTFILES $CODE/github.com/rjschie/dotfiles
   fish_add_path $HOME/.local/bin
   fish_add_path $HOME/.bun/bin
